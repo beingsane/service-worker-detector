fff16d473bae2b761357391cbd889aefb53b716c
tomayac <steiner.thomas@gmail.com>
2020-06-29 18:39:11 +0200

    Initial commit for Safari

---
+++ safari-service-worker-detector/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  safari-service-worker-detector
+//
+//  Created by Thomas Steiner on 29.06.20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
